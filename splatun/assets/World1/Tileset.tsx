d97cbc4766f198ef1beb336ba56197e7483ef960
pablomanez <pablomanez@hotmail.com>
2018-10-23 11:58:46 +0200

    Tileset parte 2

---
+++ splatun/assets/World1/Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="Tileset" tilewidth="8" tileheight="8" tilecount="32" columns="32">
+ <image source="Tileset.png" trans="000000" width="256" height="8"/>
+</tileset>
