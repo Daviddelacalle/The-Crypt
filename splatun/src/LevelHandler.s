46032844975fb86b3a21bff266b150daeb792002
Dan-sensei <datrixz997@gmail.com>
2018-10-30 19:18:03 +0100

    GNU LICENSES

--- splatun/src/LevelHandler.s
+++ splatun/src/LevelHandler.s
@@ -1,3 +1,20 @@
+;##-----------------------------LICENSE NOTICE------------------------------------
+;##  This file is part of THE CRYPT
+;##
+;##  This game is free software: you can redistribute it and/or modify
+;##  it under the terms of the GNU Lesser General Public License as published by
+;##  the Free Software Foundation, either version 3 of the License, or
+;##  (at your option) any later version.
+;##
+;##  This game is distributed in the hope that it will be useful,
+;##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;##  GNU Lesser General Public License for more details.
+;##
+;##  You should have received a copy of the GNU Lesser General Public License
+;##  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;##------------------------------------------------------------------------------
+
 
 current_level::   .db #0     ;; Offset desde el inicio de la lista de niveles
                             ;; Como cada nivel son 2 bytes, aumentará de 2 en 2
